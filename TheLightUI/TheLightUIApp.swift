802dc2681bd2d29805eff751154adb4ff56bd1a6
Nikolay Nikitin <nikitin.nikolay.v@gmail.com>
2021-07-23 21:32:30 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Николай Никитин on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
